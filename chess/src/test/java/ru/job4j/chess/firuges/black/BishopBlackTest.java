8ae0b9a11411e197c22ff57c4dbbb090c5619d4d
Scruner <aoveden1977@mail.ru>
2021-05-24 10:42:06 +0300

    2. Каркас шахматной доски [#792 #35359]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -3,6 +3,7 @@ package ru.job4j.chess.firuges.black;
 import org.junit.Test;
 import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 
 import static org.junit.Assert.*;
 
@@ -11,23 +12,28 @@ public class BishopBlackTest {
     @Test
     public void whenPosition() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        bishopBlack.position();
+        Cell expected = Cell.C1;
+        assertEquals(expected, bishopBlack.position());
     }
 
     @Test
     public void whenWay() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        Cell[] expected = {Cell.C1, Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
         assertEquals(expected, bishopBlack.way(Cell.G5));
     }
 
     @Test
     public void isDiagonal() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        boolean expected = true;
+        assertEquals(expected, bishopBlack.isDiagonal(Cell.C1, Cell.G5));
     }
 
     @Test
     public void whenCopy() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
-        bishopBlack.copy(Cell.G5);
+        BishopBlack bishopBlack = new BishopBlack(Cell.D7);
+        Cell expected = Cell.D7;
+        assertEquals(expected, bishopBlack.copy(Cell.D7));
     }
 }
